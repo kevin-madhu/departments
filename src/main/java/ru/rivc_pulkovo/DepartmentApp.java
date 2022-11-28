f123c0ec5ba015fef143f978aad4b8d4e1657774
Kevin Madhu <kevin.madhu@gmail.com>
2022-11-28 07:12:40 +0300

    Adds spring boot.

---
+++ src/main/java/ru/rivc_pulkovo/DepartmentApp.java
@@ -0,0 +1,13 @@
+package ru.rivc_pulkovo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepartmentApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DepartmentApp.class, args);
+    }
+
+}
